44197f6aadf90affc7641e49fe40cafa5178aed2
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-07-11 23:14:51 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,1 +1,12 @@
-console.log('i love git')
+const items=document.getElementsByClassName('list-group-item');
+console.log(items)
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
+const li3=document.getElementsByClassName('list-group-item');
+li3[2].style.backgroundColor='green';
+
+
+
+
