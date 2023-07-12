4dda791ee872e5a1b2fb3492b267393b0c43efc4
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-07-12 22:59:16 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,21 +1,36 @@
-const h2=document.querySelector('.title')
+const submitBtn=document.getElementById('submitBtn');
+submitBtn.addEventListener('click',addEffect);
+const ul=document.getElementById('items'); 
+const btn=document.getElementsByClassName('btn-sm');
 
-h2.parentElement.style.color='blue'
 
-const header=document.querySelector('.container')
-console.log(header.childNodes[0])
-header.children[0].style.color='red'
-console.log(header.firstElementChild)
-console.log(header.nextSibling)
-console.log(header.nextElementSibling)
+function addEffect(e){
+    e.preventDefault();
+    let input=document.getElementById('item').value;
+    const li=document.createElement('li');
+    console.log(input);
+    li.appendChild(document.createTextNode(input));
+    li.className='list-group-item';
+    ul.appendChild(li);
 
-const div1=document.createElement('div');
-const text=document.createTextNode('Hello World');
-div1.appendChild(text);
-const main=document.querySelector('#main')
-main.appendChild(div1);
-div1.setAttribute('title','hello')
-div1.className='div1'
-div1.id='divId'
-console.log(div1)
+    const newBtn=document.createElement('button');
+    newBtn.className='btn btn-danger btn-sm float-right delete';
+    newBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(newBtn);
+
+    
+}
+
+
+ul.addEventListener('click',removeItems)
+
+function removeItems(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            const li=e.target.parentElement;
+            ul.removeChild(li);
+        }
+    }
+}
+
 
