48750c7906d2c19adc7af4455617cf8b35521774
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-07-13 16:48:11 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,9 +1,67 @@
-let details={
-    'name':'Abhi',
-    'age':78
-}
-let obj_serialze=JSON.stringify(details);
-localStorage.setItem('details',obj_serialze);
+const submit=document.getElementById('submit');
+const inputName=document.getElementById('name');
+const inputEmail=document.getElementById('email');
+const inputPhone=document.getElementById('phone');
+const ul=c=document.getElementById('items');
+let del;
+let edit;
 
-let obj_deserialize=JSON.parse(localStorage.getItem('details'));
-console.log(obj_deserialize);
+class Person{
+    static count=0;
+    constructor(name,email,phone){
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        Person.count++;
+    }
+}
+
+let j=new Person('abh','@123',9876);
+console.log(j);
+
+submit.addEventListener('click',display)
+
+function display(e){
+    e.preventDefault();
+
+    const li=document.createElement('li');
+    
+    li.appendChild(document.createTextNode(`Name: ${inputName.value} Email: ${inputEmail.value} Phone: ${inputPhone.value}`));
+    li.className=inputEmail.value;
+
+    // add delete
+    del=document.createElement('button');
+    del.className='delete';
+    del.textContent='delete';
+    li.appendChild(del);
+
+    // add edit
+    edit=document.createElement('button');
+    edit.className='edit';
+    edit.textContent='edit';
+    li.appendChild(edit);
+
+    ul.appendChild(li);
+
+    
+    p=new Person(inputName.value,inputEmail.value,inputPhone.value);
+    
+    localStorage.setItem(`${inputEmail.value}`,JSON.stringify(p));
+    
+
+    inputEmail.value='';
+    inputName.value='';
+    inputPhone.value='';
+}
+
+ul.addEventListener('click',remove);
+
+function remove(e){
+    if(e.target.classList.contains('delete')){
+        const li=e.target.parentElement;
+        if(confirm('Are you sure?')){
+            ul.removeChild(li);
+            localStorage.removeItem(li.className)
+        }
+    }
+}
