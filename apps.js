dbbe1f7465c729c3c0ec3fa031ac8c58715b115c
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-07-13 13:18:37 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,36 +1,9 @@
-const myForm=document.getElementById('submit');
-const nameInput=document.getElementById('name');
-const emailInput=document.getElementById('email');
-
-
-myForm.addEventListener('click', onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
-
-    // Remove error after 3 seconds
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
-    const userList=document.getElementById('users');
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    userList.appendChild(li);
-    
+let details={
+    'name':'Abhi',
+    'age':78
 }
-localStorage.setItem('name',nameInput.value);
-    console.log(localStorage.getItem('name'))
-    localStorage.setItem('email',emailInput.value);
+let obj_serialze=JSON.stringify(details);
+localStorage.setItem('details',obj_serialze);
 
-  }
+let obj_deserialize=JSON.parse(localStorage.getItem('details'));
+console.log(obj_deserialize);
