6f43af5f25b9fc9f970632005d3538c30b77aa7c
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-07-21 00:34:06 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,82 +1,22 @@
-const submit=document.getElementById('submit');
-const inputName=document.getElementById('name');
-const inputEmail=document.getElementById('email');
-const inputPhone=document.getElementById('phone');
-const ul=c=document.getElementById('items');
-let del;
-let edit;
+// let posts=[
+  {title:'post one'},
+  {title:'post two'}
+]
 
-class Person{
-    static count=0;
-    constructor(name,email,phone){
-        this.name=name;
-        this.email=email;
-        this.phone=phone;
-        Person.count++;
-    }
+function createPost(post){
+  return new Promise((resolve,request)=>{
+    posts.push(post);
+    updateLastUserActivity();
+    resolve();
+  })
 }
 
-let j=new Person('abh','@123',9876);
-console.log(j);
-
-submit.addEventListener('click',display)
-
-function display(e){
-    e.preventDefault();
-
-    const li=document.createElement('li');
-    
-    li.appendChild(document.createTextNode(`Name: ${inputName.value} Email: ${inputEmail.value} Phone: ${inputPhone.value}`));
-    li.className=inputEmail.value;
-
-    // add delete
-    del=document.createElement('button');
-    del.className='delete';
-    del.textContent='delete';
-    li.appendChild(del);
-
-    // add edit
-    edit=document.createElement('button');
-    edit.className='edit';
-    edit.textContent='edit';
-    li.appendChild(edit);
-
-    ul.appendChild(li);
-
-    
-    p=new Person(inputName.value,inputEmail.value,inputPhone.value);
-    
-    localStorage.setItem(`${inputEmail.value}`,JSON.stringify(p));
-    
-
-    inputEmail.value='';
-    inputName.value='';
-    inputPhone.value='';
+function updateLastUserActivity(){
+  return new Promise((resolve,request)=>{
+    let newDate=new Date().getTime();
+    resolve(newDate);
+  })
 }
-
-ul.addEventListener('click',remove);
-
-function remove(e){
-    if(e.target.classList.contains('delete')){
-        const li=e.target.parentElement;
-        if(confirm('Are you sure?')){
-            ul.removeChild(li);
-            localStorage.removeItem(li.className)
-        }
-    }
-}
-
-ul.addEventListener('click',editForm);
-
-function editForm(e){
-    if(e.target.classList.contains('edit')){
-        let li=e.target.parentElement;
-        let obj=JSON.parse(localStorage.getItem(li.className));
-        localStorage.removeItem(li.className);
-        ul.removeChild(li);
-        
-        inputEmail.value=obj.email;
-        inputPhone.value=obj.phone;
-        inputName.value=obj.name;
-    }
-}
+Promise.all([createPost({title:'post three'},{title:'post four'}),updateLastUserActivity()]).then(([value1,value2])=>{
+  console.log(value2);
+})
