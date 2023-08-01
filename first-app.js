c904f65a99058e9369c15152501e6e50c931f251
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-08-01 14:35:44 +0530

    Update first-app.js
    arrow function,object

--- first-app.js
+++ first-app.js
@@ -1,3 +1,16 @@
 
 const fs=require('fs');
 fs.writeFileSync('hello.txt','console.log(first node project)');
+
+const product=(a,b)=> a*b;
+console.log(product(8,5));
+
+const Student={
+    name:'Abhi',
+    address:'Bangalore',
+    details:function(){
+        console.log(`name:${this.name},address:${this.address}`);
+    }
+}
+
+Student.details();
