600379932a9c0ed51dd82fea6f6ae5bbb138bf29
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-08-06 20:51:38 +0530

    Update first-app.js

--- first-app.js
+++ first-app.js
@@ -1,12 +1,24 @@
 const express=require('express');
 const app=express();
 
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/add-product',(req,res,next)=>{
-    console.log('In the middlewear');
-    res.send('<h1>Hello World<h1/>');
-   
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
+//    next();
+})
+
+app.use('/product',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
 
 app.use('/',(req,res,next)=>{
-    console.log('another');
+    res.send('<h1>Hello World</h1>');
 })
+
+// let server=http.createServer(app);
+
+app.listen(4001);
