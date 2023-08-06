28861019b5ab7fbb2d20d3da0cfca81310f7cd43
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-08-06 23:54:25 +0530

    Update first-app.js
    ROUTE

--- first-app.js
+++ first-app.js
@@ -1,24 +1,46 @@
+// first-app file
 const express=require('express');
 const app=express();
+const adminRoute=require('./Routes/admin');
+const shopRoute=require('./Routes/shop');
+
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
-//    next();
+app.use('/admin',adminRoute);
+
+app.use('/admin',shopRoute);
+
+// let server=http.createServer(app);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>error not found</h1>');
 })
 
-app.use('/product',(req,res)=>{
+app.listen(4001);
+
+// ADMIN FILE
+const express=require('express');
+const router=express.Router();
+
+router.get('/add-product',(req,res,next)=>{  
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
+})
+
+router.post('/product',(req,res)=>{
     console.log(req.body);
     res.redirect('/');
 })
 
-app.use('/',(req,res,next)=>{
+module.exports=router;
+
+// SHOP FILE
+const express=require('express');
+
+const router=express.Router();
+
+router.get('/',(req,res,next)=>{
     res.send('<h1>Hello World</h1>');
 })
 
-// let server=http.createServer(app);
-
-app.listen(4001);
+module.exports=router;
