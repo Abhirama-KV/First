5d68c534f9f5bd2fb116d2fe73a6d3f531b9b1f5
Abhirama-KV <55393923+Abhirama-KV@users.noreply.github.com>
2023-08-15 01:22:30 +0530

    Update first-app.js
    GROUP CHAT USING FILES

--- first-app.js
+++ first-app.js
@@ -1,4 +1,4 @@
-// first-app file
+// sirst-app
 const express=require('express');
 const app=express();
 const adminRoute=require('./Routes/admin');
@@ -8,9 +8,9 @@ const shopRoute=require('./Routes/shop');
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/admin',adminRoute);
+app.use(adminRoute);
 
-app.use('/admin',shopRoute);
+app.use(shopRoute);
 
 // let server=http.createServer(app);
 app.use((req,res,next)=>{
@@ -19,12 +19,12 @@ app.use((req,res,next)=>{
 
 app.listen(4001);
 
-// ADMIN FILE
+// admin js
 const express=require('express');
 const router=express.Router();
 
 router.get('/add-product',(req,res,next)=>{  
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
+    res.send('<form action="product" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form>');
 })
 
 router.post('/product',(req,res)=>{
@@ -32,15 +32,53 @@ router.post('/product',(req,res)=>{
     res.redirect('/');
 })
 
+router.get('/login',(req,res,next)=>{
+    res.send(`<form action="/open" onsubmit="localStorage.setItem('username',document.getElementById('user').value)" method="POST"><input type="username" id="user"><button type="submit">login</button></form>`);
+})
+
+router.post('/open',(req,res,next)=>{
+    
+    console.log(`<h1>Hello from open</h1>`)
+    res.redirect('/');
+});
+
 module.exports=router;
 
-// SHOP FILE
+// shop js
 const express=require('express');
+const fs=require('fs');
 
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Hello World</h1>');
+    fs.readFile('msgs.txt',(err,data)=>{        //read file
+        if(err){
+            data='NO chat exists'
+        }
+        res.send(` ${data}<form action="/" onsubmit="document.getElementById('userName').value=localStorage.getItem('username')" method="POST">
+        <input type="hidden" id="userName" name="userName">
+        <input type="text" id="message" name="message">
+        <button type="submit">Send</button>   
+    </form>`);
+    })  
+  
+})
+
+router.post('/',(req,res,next)=>{
+    fs.appendFile('msgs.txt',` ${req.body.userName}:${req.body.message}`,(err)=>{      //write filr
+        if(err){
+            console.log(err);
+            data=err;
+        }
+
+    });
+    console.log(`${req.body.userName}:${req.body.message} hello`);
+    // res.send(`<h3>${req.body.userName}:${req.body.message}</h3>`);
+    res.redirect('/');
+
+    
 })
 
 module.exports=router;
+
+
